768ec5585a2e18d12d4108f5b6bf5a5a5641101d
Rahul Singh <rahullsingh506@gmail.com>
2025-11-25 15:37:11 +0530

    deletFolder

--- PROJECT/University_Assignment_Approval_System/models/Department.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const DepartmentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  type: { type: String, enum: ["UG", "PG", "Research"], required: true },
-  address: { type: String, required: true }
-});
-
-module.exports = mongoose.models.Department || mongoose.model("Department", DepartmentSchema);
